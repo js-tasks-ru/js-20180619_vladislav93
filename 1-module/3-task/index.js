f39f367dc79da8170b8099f9ba0aaaba13debfc6
Vladislav <ckpol1112@gmail.com>
2018-07-18 21:41:01 +0300

    Решена задача модуля 1 #3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,31 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+    function isNumber (n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
 
-}
+    function foo(a,b) {
+        return a - b;
+    }
 
+    function string (str) {
+        return +str;
+    }
+
+    function getMinMax(str) {
+    let arr = str.split(/[, ]/);
+    let arrNum = arr.map(string).filter(isNumber).sort(foo);
+
+    let max = arrNum[arrNum.length - 1];
+    let min = arrNum[0];
+
+    let obj = {
+        'min':min,
+        'max':max
+    };
+
+        return  obj;
+    }
+
+    let inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
